85af4f18e9417f9675067028278e34d88cfb8ee5
tomsithCGAT <tsmith@mrc-lmb.cam.ac.uk>
2024-11-08 10:27:37 +0000

    adds function to parse prot desc col

---
+++ R/add_gene_protein_name.R
@@ -0,0 +1,46 @@
+#' Extract the gene name and long-form protein name from the master protein descriptions column
+#'
+#' @description This function extracts the gene name and long-form protein name
+#' from the master protein descriptions column in output from
+#' Proteome Discoverer for DDA.
+#'
+#' It assumes the master protein description column is in the format
+#' (.*) .* GN=(.*) PE.*, where the first group is the long format of the protein
+#' name and the second group is the gene name. Where the gene name is not included,
+#' an empty string is returned
+#' An example of the expected input in the column is:
+#' Serum albumin OS=Bos taurus GN=ALB PE=1 SV=4
+#'
+#'
+#' ```
+#' @param obj `SummarisedExperiment` containing output from Proteome Discoverer
+#' @param master_protein_desc_col `string`. Name of column containing master
+#' proteins descriptions.
+#' @param gene_name_out_col `string`. Name of output column containing the gene names
+#' @param long_protein_name_out_col `string`. Name of output column containing the long format
+#' protein names
+
+#' @return Returns a `QFeatures` with the filtered Proteome Discoverer output.
+#' @export
+add_gene_long_protein_name_pd <- function(obj,
+                                          master_protein_desc_col='Master.Protein.Descriptions',
+                                          gene_name_out_col='Master.Protein.gene.name',
+                                          long_protein_name_out_col='Master.Protein.long.name'
+         ){
+
+  check_se(obj)
+
+  rowData(obj)$Master.Protein.gene.name <-
+    sapply(strsplit(rowData(obj)[[master_protein_desc_col]], ';'),
+           function(x) paste(
+             ifelse(grepl('GN=', x),gsub('.* GN=(.*) PE.*', '\\1', x), ''),
+             collapse=';'))
+
+  rowData(obj)$Master.Protein.long.name <-
+    sapply(strsplit(rowData(obj)[[master_protein_desc_col]], ';'),
+           function(x) paste(
+             gsub(' OS=.*', '', x),
+             collapse=';'))
+
+  return(obj)
+}

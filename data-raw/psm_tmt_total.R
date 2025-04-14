ae43d80931f9fb258f03b473f65e7e752d0a0019
tomsithCGAT <tsmith@mrc-lmb.cam.ac.uk>
2025-04-14 12:36:27 +0100

    debug vignettes after changing parse functions

--- data-raw/psm_tmt_total.R
+++ data-raw/psm_tmt_total.R
@@ -1,9 +1,10 @@
 # Load libraries
 library(camprotR)
-
+library(dplyr)
 # psm_tmt_total is part of the camprotR package which we will reuse here for now
 
 # Output .rda file
-usethis::use_data(psm_tmt_total, overwrite = TRUE)
-
+psm_tmt_total <- psm_tmt_total %>%
+  mutate(Quan.Info=ifelse(is.na(Quan.Info), '', Quan.Info))
+ usethis::use_data(psm_tmt_total, overwrite = TRUE)
 

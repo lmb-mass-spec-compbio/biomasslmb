cb23f6d6b0e360dd12c8497ac76a1b7d4e28d1c2
tomsithCGAT <tsmith@mrc-lmb.cam.ac.uk>
2025-06-25 23:08:29 +0100

    adds function to handle updated maxquant ptm encoding

--- R/ptm.R
+++ R/ptm.R
@@ -22,8 +22,8 @@ filter_maxquant_ptm <- function(obj, i, ptm_prob_col='Phospho..STY..Probabilitie
 #' @return Returns a `SummarizedExperiment` with an additional column in the RowData describing the position of the PTMs with respect to the peptide sequence
 #' @export
 add_ptm_pos_rowdata_mq <- function(obj,
-                                   ptms_to_retain = c('p'),
-                                   ptm_encoding_pos = c('p'=-1, '(ox)'=1),
+                                   ptms_to_retain = c('p', '(ph)', '(ac)p'),
+                                   ptm_encoding_pos = c('p'=-1, '(ph)'=1, '(ac)p'=-1, '(ox)'=1),
                                    verbose=TRUE){
 
 
@@ -106,6 +106,96 @@ add_ptm_pos_rowdata_mq <- function(obj,
 }
 
 
+#' Add rowData columns with positions of PTMs with respect to peptide sequence
+#'
+#' @description This function takes a `SummarizedExperiment` object with the evidence.txt
+#' output from MaxQuant and adds rowData columns to describe the PTMs present in the peptide and their positions within the peptide' '
+#' @param obj `SummarizedExperiment`. Proteomics dataset
+#' @param ptms_to_retain `character` vector with PTMs to retain. Inspect values in rowData(obj)$Modified.sequence to get correct PTM name
+#' @param ptm_encoding_pos `character` name vector describing whether modifification comes before (-1) or after (1) amino acid in Modified.sequence column values.
+#' @return Returns a `SummarizedExperiment` with an additional column in the RowData describing the position of the PTMs with respect to the peptide sequence
+#' @export
+#' @export
+add_filter_ptm_pos_rowdata_mq <- function(obj,
+         ptms_to_retain = c('Phospho (STY)'),
+         prob_col = 'Phospho..STY..Probabilities',
+         min_prob=0.501,
+         filter_pep_by_prob=TRUE,
+         verbose=TRUE){
+
+  mod_seq <- gsub("_$|^_", "", rowData(obj)$Modified.sequence)
+  mod_matches <- gregexpr('\\(.*?\\)\\)', mod_seq)
+  mods <- regmatches(mod_seq, mod_matches)
+  pep_split <- regmatches(mod_seq, mod_matches, invert = TRUE)
+
+  prob_matches <- gregexpr('\\((0.\\d+|1)\\)', rowData(obj)[[prob_col]])
+  mod_probs <- regmatches(rowData(obj)[[prob_col]], prob_matches)
+  pep_split_mod <- regmatches(rowData(obj)[[prob_col]], prob_matches, invert = TRUE)
+
+  ptms_detected <- list()
+
+  ptm_pos <- lapply(1:length(mod_seq), function(seq_ix){
+
+    seq_mods <- mods[[seq_ix]]
+    names(seq_mods) <- head(cumsum(nchar(pep_split[[seq_ix]])), -1)
+
+    seq_probs <- as.numeric(gsub('(\\(|\\))', '', mod_probs[[seq_ix]]))
+    names(seq_probs) <- head(cumsum(nchar(pep_split_mod[[seq_ix]])), -1)
+
+    ptm_positions <- c()
+    ptms <- c()
+    ptm_amino_acids <- c()
+    n_ptms_detected <- 0
+
+    for(ptm_pos in names(seq_mods)){
+
+      seq_mod <- gsub('(^\\(|\\)$)', '', seq_mods[[ptm_pos]])
+
+      if(seq_mod %in% names(ptms_detected)){
+        ptms_detected[[seq_mod]] <<- ptms_detected[[seq_mod]] + 1
+      } else{
+        ptms_detected[[seq_mod]] <<- 1
+      }
+
+      if(seq_mod %in% ptms_to_retain){
+        n_ptms_detected <- n_ptms_detected + 1
+
+        mod_prob <- seq_probs[ptm_pos]
+
+        if(mod_prob>=min_prob){
+          ptms <- c(ptms, seq_mod)
+          ptm_positions <- c(ptm_positions, ptm_pos)
+          ptm_amino_acids <- c(ptm_amino_acids, substr(rowData(obj)$Sequence[[seq_ix]], as.numeric(ptm_pos), as.numeric(ptm_pos)))
+        }
+      }
+    }
+
+    n_ptms = length(ptms)
+    ptms <- paste(ptms, collapse = '; ')
+    ptm_positions <- paste(ptm_positions, collapse = '; ')
+    ptm_amino_acids <- paste(ptm_amino_acids, collapse = '; ')
+
+    return(c('ptms'=ptms, 'ptm_positions'=ptm_positions, 'ptm_amino_acids'=ptm_amino_acids,
+             'n_ptms'=n_ptms, 'n_ptms_detected'=n_ptms_detected))
+  }) %>% bind_rows()
+
+
+  if(verbose){
+    ptms_detected <- ptms_detected[order(-as.numeric(ptms_detected))]
+    ptms_detected_str <- paste(sapply(names(ptms_detected), function(x) sprintf('%s: %s', x, ptms_detected[[x]])), collapse='; ')
+
+    message(sprintf('The following PTMs were detected: %s', ptms_detected_str))
+  }
+
+  rowData(obj) <- bind_cols(data.frame(rowData(obj)), ptm_pos)
+
+  if(filter_pep_by_prob){
+    obj <- obj[rowData(obj)$n_ptms==rowData(obj)$n_ptms_detected,]
+  }
+  return(obj)
+}
+
+
 #' Add peptide positions with respect to the protein
 #'
 #' @description This function takes a `SummarizedExperiment` object and adds rowData
@@ -136,7 +226,7 @@ add_peptide_positions_from_cleavage <- function(obj,
 
   pep_ranges <- cleaver::cleavageRanges(proteins, enzym=digest_enzyme, missedCleavages = missed_cleavages)
 
-  pep_loc <- names(pep_ranges) %>% lapply(function(x){
+  pep_loc <- unique(names(pep_ranges)) %>% lapply(function(x){
     .df <- data.frame(pep_ranges[[x]]) %>%
       mutate(sequence=as.character(digest_pep[[x]]),
              width=width(digest_pep[[x]]))

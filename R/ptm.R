6b1d5915fdda1994132c1207f86f2e0a4883b447
tomsithCGAT <tsmith@mrc-lmb.cam.ac.uk>
2025-07-10 12:32:27 +0100

    adds function to parse PTM scores

--- R/ptm.R
+++ R/ptm.R
@@ -27,6 +27,8 @@ add_ptm_pos_rowdata_mq <- function(obj,
                                    verbose=TRUE){
 
 
+  check_se(obj)
+
   ptms_without_defined_encoding_pos <- setdiff(ptms_to_retain, names(ptm_encoding_pos))
 
   if(length(ptms_without_defined_encoding_pos)>0){
@@ -123,6 +125,8 @@ add_filter_ptm_pos_rowdata_mq <- function(obj,
          filter_pep_by_prob=TRUE,
          verbose=TRUE){
 
+  check_se(obj)
+
   mod_seq <- gsub("_$|^_", "", rowData(obj)$Modified.sequence)
   mod_matches <- gregexpr('\\(.*?\\)\\)', mod_seq)
   mods <- regmatches(mod_seq, mod_matches)
@@ -217,6 +221,8 @@ add_peptide_positions_from_cleavage <- function(obj,
                                                 missed_cleavages = c(0,1,2),
                                                 master_protein_col = "Leading.razor.protein",
                                                 sequence_col = "Sequence") {
+  check_se(obj)
+
   proteins <- Biostrings::readAAStringSet(proteome_fasta)
 
   # Update name to be just UniprotID
@@ -302,3 +308,139 @@ add_ptm_positions <- function(obj,
   return(obj)
 }
 
+
+#' Parse the PTM probabilities from Proteome Discoverer and add new columns with PTM information
+#'
+#' @description Extract PTM information from the PTM probabilities column in
+#' the output from PD and add new columns with this information. Also optionally
+#' prints a summary of how many features (PSMs/peptides) pass a given probability
+#' threshold
+#'
+#' @param obj `SummarizedExperiment`. Proteomics dataset
+#' @param threshold `numeric` If any score is below a set threshold, disregard all putative PTM sites
+#' @param ptm_col `character` Columm name for PTM probabilities
+#' @param prob_split `character` regex to split PTM probabilities
+#' @param collapse_delimiter `character` delimiter for multiple values in output columns
+#' @param verbose Set TRUE to print log of events to console
+#'
+#' @return `SummarizedExperiment`
+#' @export
+parse_PTM_scores_pd <- function(obj,
+                                threshold = 95,
+                                ptm_col = "ptmRS.Best.Site.Probabilities",
+                                prob_split = '; |: ',
+                                collapse_delimiter = "; ",
+                                verbose = TRUE) {
+
+  check_se(obj)
+  new_obj <- obj
+
+  if(verbose){
+    message(sprintf(
+      "Removed %s Features where the ptm_col value == `Inconclusive data`",
+      sum(rowData(new_obj)[[ptm_col]] == "Inconclusive data")
+    ))
+  }
+
+  new_obj <- new_obj[rowData(new_obj)[[ptm_col]] != "Inconclusive data", ]
+
+  # initiate vectors with empty string
+  # where the PTM(s) pass the threshold, these will be updated
+  filtered_ptm_desc <- filtered_ptm_res <- filtered_ptm_pos <- filtered_ptm <- filtered_ptm_score <- rep("", nrow(rowData(new_obj)))
+
+  split_probabities <- strsplit(rowData(new_obj)[[ptm_col]], split = prob_split)
+
+  log <- list(
+    "Total Features" = 0,
+    "Total detected PTMFeatures" = 0,
+    "Features passing filter" = 0,
+    "Features failing filter" = 0,
+    "BiPTM/multiPTM Features where some sites fail filter" = 0,
+    "Total detected sites" = 0,
+    "Sites passing filter" = 0,
+    "Sites failing filter" = 0,
+    "monoPTM passing filter" = 0,
+    "biPTM passing filter" = 0,
+    "multiPTM passing filter" = 0,
+    "Too many isoforms" = 0
+  )
+
+  for (i in seq_along(split_probabities)) {
+    log[["Total Features"]] <- log[["Total Features"]] + 1
+
+    peptide_ptm_scores <- split_probabities[[i]]
+    if (length(peptide_ptm_scores) == 0) {
+      next()
+    } # no PTM detected
+    if (is.na(peptide_ptm_scores[[1]])) {
+      next()
+    } # no PTM detected
+
+    log[["Total detected PTMFeatures"]] <- log[["Total detected PTMFeatures"]] + 1
+
+    if (peptide_ptm_scores[[1]] == "Too many isoforms") {
+      log[["Too many isoforms"]] <- log[["Too many isoforms"]] + 1
+      log[["Features failing filter"]] <- log[["Features failing filter"]] + 1
+      next()
+    }
+
+    log[["Total detected sites"]] <- log[["Total detected sites"]] + length(peptide_ptm_scores) / 2
+
+    scores <- peptide_ptm_scores[seq(2, length(peptide_ptm_scores), 2)]
+
+    # if any score is below threshold, disregard all putative ptm sites
+    if (any(as.numeric(scores) < threshold)) {
+      log[["Sites failing filter"]] <- log[["Sites failing filter"]] + length(peptide_ptm_scores) / 2
+      log[["Features failing filter"]] <- log[["Features failing filter"]] + 1
+      if (any(as.numeric(scores) >= threshold)) {
+        log[["BiPTM/multiPTM Features where some sites fail filter"]] <- log[[
+          "BiPTM/multiPTM Features where some sites fail filter"]] + 1
+      }
+      # if we want to handle this differently, can implement an alternative approach here
+      # and move the rest of the code below into an else clause
+      next()
+    }
+
+    log[["Sites passing filter"]] <- log[["Sites passing filter"]] + length(peptide_ptm_scores) / 2
+    log[["Features passing filter"]] <- log[["Features passing filter"]] + 1
+
+    if (length(scores) == 1) {
+      log[["monoPTM passing filter"]] <- log[["monoPTM passing filter"]] + 1
+    }
+    else if (length(scores) == 2) {
+      log[["biPTM passing filter"]] <- log[["biPTM passing filter"]] + 1
+    }
+    else {
+      log[["multiPTM passing filter"]] <- log[["multiPTM passing filter"]] + 1
+    }
+
+    ptms <- peptide_ptm_scores[seq(1, length(peptide_ptm_scores), 2)] # extract the PTMs info
+    split_ptms <- unlist(strsplit(ptms, split = '\\(|\\)')) # split to remove parantheses
+    modifications <- split_ptms[seq(2, length(split_ptms), 2)] # extract modifications, e.g "phospho"
+    positions <- split_ptms[seq(1, length(split_ptms), 2)] # extract the positions, e.g "S6"
+    residues <- substr(positions, 1, 1) # extract first element, e.g S
+    positions <- sub('.', '', positions) # remove first element and leave position, e.g 6
+
+    # paste together the value, separated by option(collapse_delimiter) and update vectors which will become columns
+    #filtered_ptm_desc[[i]] <- paste(peptide_ptm_scores, collapse = collapse_delimiter)
+    filtered_ptm_res[[i]] <- paste(residues, collapse = collapse_delimiter)
+    filtered_ptm_pos[[i]] <- paste(positions, collapse = collapse_delimiter)
+    filtered_ptm[[i]] <- paste(modifications, collapse = collapse_delimiter)
+    filtered_ptm_score[[i]] <- paste(scores, collapse = collapse_delimiter)
+  }
+
+  # add columns
+  #rowData(new_obj)['filtered_PTM_desc'] = filtered_ptm_desc
+  rowData(new_obj)['ptm_amino_acids'] = filtered_ptm_res
+  rowData(new_obj)['ptm_positions'] = filtered_ptm_pos
+  rowData(new_obj)['ptms'] = filtered_ptm
+  rowData(new_obj)['ptm_scores'] = filtered_ptm_score
+
+  if (verbose) {
+    for (event in names(log)) {
+      message(sprintf("%s: %i", event, log[[event]]))
+    }
+  }
+
+  return(new_obj)
+}
